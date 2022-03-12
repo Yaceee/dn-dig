46a8a9d24a0bc8a9640bacd3f13d74e5013d8bbe
Adrial-Knight <pierreminier64@gmail.com>
2022-03-12 19:58:03 +0100

    [Evaluation DB] allocation tableau

--- sources/evaluation_DB.py
+++ sources/evaluation_DB.py
@@ -1,5 +1,5 @@
 from os import path, listdir
-from numpy import shape, mean
+import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.ticker as mticker
 
@@ -12,10 +12,11 @@ def im_load(folder):
     names = listdir(folder)
     names.sort()
 
-    images = []
+    N = len(names)
+    images = [plt.imread(folder + "/" + names[0])[:, :, 0:3]] * N
 
-    for name in names:
-        images.append(plt.imread(folder + "/" + name)[:, :, 0:3])
+    for i in range(1, N):
+        images[i] = plt.imread(folder + "/" + names[i])[:, :, 0:3]
 
     return images, names
 
@@ -23,20 +24,25 @@ def im_load(folder):
 def check_pair(id_days, id_nights):
     Nd = len(id_days)
     Nn = len(id_nights)
+
     if Nd != Nn:
         print(f"[Mismatching numbers]\tday: {Nd} /--/ night: {Nn}")
         exit(1)
 
     for i in range(Nd):
         if id_days[i] != id_nights[i]:
-            print(f"[Mismatching images]\tday: {id_days[i]} /--/ night: {id_nights[i]}")
+            print(
+                f"[Mismatching images]\tday: {id_days[i]} \
+                /--/ night: {id_nights[i]}"
+            )
 
     return 0
 
 
 def score_im(day, night):
     score = 0
-    w, h, c = shape(day)
+    w, h, _ = np.shape(day)
+
     for x in range(w):
         for y in range(h):
             score += not any(day[x, y, :] != night[x, y, :])
@@ -47,9 +53,10 @@ def score_im(day, night):
 
 def score_database(days, nights):
     N = len(days)
-    scores = []
+    scores = np.zeros(N, dtype=float)
+
     for i in range(N):
-        scores.append(score_im(days[i], nights[i]))
+        scores[i] = score_im(days[i], nights[i])
 
     return scores
 
@@ -60,12 +67,12 @@ if __name__ == "__main__":
 
     check_pair(id_days, id_nights)
 
-    score = score_database(days, nights)
+    scores = score_database(days, nights)
 
-    plt.plot(range(1, len(score) + 1), score)
+    plt.plot(range(1, len(scores) + 1), scores)
     plt.gca().xaxis.set_major_locator(mticker.MultipleLocator(1))
     plt.ylim(0, 1.05)
     plt.xlabel("frames", fontsize=12)
     plt.ylabel("likeness", fontsize=12)
-    plt.title(f"Dataset score: {mean(score)}", fontsize=18)
+    plt.title(f"Dataset score: {np.mean(scores)}", fontsize=18)
     plt.show()
