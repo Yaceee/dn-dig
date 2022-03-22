738b9d9439fae02c88465755abc9adeb399565c0
Adrial-Knight <pierreminier64@gmail.com>
2022-03-22 18:15:31 +0100

    [Synchro + voitures] meilleurs synchro, début de l'ajout du traffic

---
+++ sources/simulation.py
@@ -0,0 +1,121 @@
+import daynightdl as dn
+import config as conf
+import sys
+
+from tqdm import tqdm
+from queue import Queue, Empty
+from time import sleep
+
+
+def simulation(is_sun, confObj):
+    try:
+        # ---------------------------------------------------------------------
+        # INITIALIZATION
+        #
+        # client
+        client = dn.carla.Client(confObj.host, confObj.port)
+        client.set_timeout(10.0)
+
+        # load the specified world
+        try:
+            world = client.load_world(conf.TOWN_ID)
+        except RuntimeError:
+            print(f"{conf.TOWN_ID} not found, use the current world")
+            world = client.get_world()
+
+        # set world mode to synchronous
+        world.apply_settings(
+                dn.carla.WorldSettings(synchronous_mode=True,
+                                       no_rendering_mode=False,
+                                       fixed_delta_seconds=0.05))
+
+        # Set up the traffic manager
+        traffic_manager = client.get_trafficmanager(conf.TM_PORT)
+        traffic_manager.set_synchronous_mode(True)
+        traffic_manager.set_random_device_seed(conf.TM_SEED)
+
+        # weather
+        dn.IMAGE_FOLDER = "DAY" if is_sun else "NIGHT"
+        dn.set_weather(world, is_sun=is_sun)
+
+        # car configuration
+        vehicle_list = []
+        vehicle = dn.set_autonom_car(world, tag="model3", tm_port=conf.TM_PORT)
+        traffic_manager.ignore_lights_percentage(vehicle, 100)
+        vehicle_list.append(vehicle)
+
+        # attach cameras to the vehicle
+        sensor_list = []
+        sensor_queue = Queue()
+
+        cam_semantic = dn.camera_init(
+            "sensor.camera.semantic_segmentation",
+            confObj.segTag,
+            world,
+            vehicle,
+            sensor_queue,
+            confObj
+        )
+        cam_rgb = dn.camera_init(
+            "sensor.camera.rgb", confObj.rgbTag, world, vehicle, sensor_queue, confObj
+        )
+
+        sensor_list.append(cam_semantic)
+        sensor_list.append(cam_rgb)
+
+        # to do:
+        # put the following code to DayNightDL in set_autonom_car
+        # add a percentage for traffic and return the vehicle list
+        # the first one will have the cameras
+
+        spawn_list = world.get_map().get_spawn_points()[1:]
+        blueprint_library = world.get_blueprint_library()
+        vehicle_bp = blueprint_library.filter("model3")[0]
+
+        if dn.IMAGE_FOLDER == "NIGHT":
+            current_lights = dn.carla.VehicleLightState.All
+        else:
+            current_lights = dn.carla.VehicleLightState.NONE
+
+        for spawn in spawn_list:
+            vehicle = world.spawn_actor(vehicle_bp, spawn)
+            vehicle.set_light_state(current_lights)
+            vehicle.set_autopilot(True, conf.TM_PORT)
+            vehicle_list.append(vehicle)
+
+        # end to do
+
+        # ---------------------------------------------------------------------
+        # SIMULATION
+        #
+        world.tick()
+        print(f"Recording {dn.IMAGE_FOLDER} images")
+        for dn.frame_id in tqdm(range(1, confObj.imNum+1)):
+            try:
+                for _ in range(len(sensor_list)):
+                    s_frame = sensor_queue.get(block=True, timeout=1)
+            except Empty:
+                continue
+            for _ in range(20):
+                world.tick()
+
+        sleep(2) # allow time to save the last image
+        #
+        # ---------------------------------------------------------------------
+
+    # -------------------------------------------------------------------------
+    # CLEANING
+    #
+    finally:
+        world.apply_settings(dn.carla.WorldSettings(False, False, 0))
+        for vehicle in vehicle_list:
+            vehicle.destroy()
+        for sensor in sensor_list:
+            sensor.stop()
+            sensor.destroy()
+        print("Server cleaned")
+
+
+if __name__ == '__main__':
+    is_sun = True if sys.argv[1] == "sun" else False
+    simulation(is_sun, conf.globalConf)
