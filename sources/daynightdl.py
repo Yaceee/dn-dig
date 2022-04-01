0570d3b12f9f1fde91a02504983151ccadb2c5ea
Adrial-Knight <pierreminier64@gmail.com>
2022-04-02 00:10:17 +0200

    [Arg parser] arguments pris en ligne de commande grâce à argparse

--- sources/daynightdl.py
+++ sources/daynightdl.py
@@ -7,7 +7,6 @@ Librairie générale pour le projet DayNightDL
 import glob
 import os
 import sys
-import random
 
 try:
     sys.path.append(
@@ -27,35 +26,49 @@ import carla
 IMAGE_FOLDER = None
 frame_id = None
 
-def camera_init(tag, world, vehicle, queue, confObj):
+VEHICLE_ID = ["a2", "impala", "c3", "microlino", "charger_police", "tt", "wrangler_rubicon", "coupe", "coupe_2020", "low_rider", "charger_2020", "ambulance", "mkz_2020", "mini", "prius", "crown", "carlacola", "zx125", "nissan",
+              "charger_police_2020", "sprinter", "etron", "leon", "t2_2021", "cybertruck", "mkz_2017", "mustang", "carlamotors", "volkswagen", "tesla", "century", "omafiets", "grandtourer", "crossbike", "ninja", "yzf", "patrol", "micra", "cooper_s"]
+
+
+def camera_init(tag, world, vehicle, queue, config):
     # config the blueprint
-    blueprint_library = world.get_blueprint_library()
+    blueprint_lib = world.get_blueprint_library()
     if tag == "seg":
-        camera_bp = blueprint_library.find("sensor.camera.semantic_segmentation")
+        camera_bp = blueprint_lib.find("sensor.camera.semantic_segmentation")
     elif tag == "rgb":
-        camera_bp = blueprint_library.find("sensor.camera.rgb")
+        camera_bp = blueprint_lib.find("sensor.camera.rgb")
     else:
         return None
 
-    camera_bp.set_attribute("image_size_x", f"{confObj.width}")
-    camera_bp.set_attribute("image_size_y", f"{confObj.height}")
+    camera_bp.set_attribute("image_size_x", f"{config.dimension[0]}")
+    camera_bp.set_attribute("image_size_y", f"{config.dimension[1]}")
     camera_bp.set_attribute("sensor_tick", "1")
-    camera_bp.set_attribute("fov", f"{confObj.fov}")
+    camera_bp.set_attribute("fov", f"{config.fov}")
 
     # pick and place
-    spawn_point = carla.Transform(carla.Location(x=2.5, y=0, z=0.7),
-                                  carla.Rotation(roll=0, pitch=0, yaw=0))
+    spawn_point = carla.Transform(carla.Location(
+                                                    x=config.position[0],
+                                                    y=config.position[1],
+                                                    z=config.position[2]
+                                                ),
+                                  carla.Rotation(
+                                                    roll=config.rotation[0],
+                                                    pitch=config.rotation[1],
+                                                    yaw=config.rotation[2]
+                                                )
+                                  )
     camera = world.spawn_actor(camera_bp, spawn_point, attach_to=vehicle)
 
     # camera action defined by the sensor callback
-    camera.listen(lambda data: sensor_callback(data, queue, tag, confObj))
+    camera.listen(lambda data: sensor_callback(data, queue, tag, config))
 
     return camera
 
 
-def sensor_callback(image, sensor_queue, tag, confObj):
-    path = f"../DB/{IMAGE_FOLDER}/{tag}/{confObj.town}_{confObj.sim}_{frame_id}.png"
-    if tag == confObj.segTag:
+def sensor_callback(image, sensor_queue, tag, config):
+    path = f"../{config.dbname}/{IMAGE_FOLDER}/{tag}/ \
+                {config.town}_{config.tag}_{frame_id}.png"
+    if tag == "seg":
         image.save_to_disk(path, carla.ColorConverter.CityScapesPalette)
     else:
         image.save_to_disk(path)
@@ -63,19 +76,18 @@ def sensor_callback(image, sensor_queue, tag, confObj):
     sensor_queue.put((image.frame, image))
 
 
-def set_weather(world, is_sun, confObj):
-    angle = confObj.sun if is_sun else confObj.moon
-    weather = carla.WeatherParameters(sun_altitude_angle=angle)
+def set_weather(world, config):
+    weather = carla.WeatherParameters(sun_altitude_angle=config.angle)
     world.set_weather(weather)
 
 
-def set_autonom_car(world, confObj, tm_port):
+def set_autonom_car(world, config, tm_port):
     # config the blueprint
     blueprint_library = world.get_blueprint_library()
 
     # select enought spawn points
     spawn_list = world.get_map().get_spawn_points()
-    nb_vehicle = round(confObj.traffic / 100 * (len(spawn_list)-1)) + 1
+    nb_vehicle = round(config.traffic / 100 * (len(spawn_list)-1)) + 1
     spawn_list = spawn_list[0:nb_vehicle]
 
     vehicle_list = [0] * nb_vehicle
@@ -86,9 +98,9 @@ def set_autonom_car(world, confObj, tm_port):
         lights = carla.VehicleLightState.NONE
 
     i = 0
-    nb_model = len(confObj.vehicle_id)
+    nb_model = len(VEHICLE_ID)
     for spawn in spawn_list:
-        id = confObj.vehicle_id[i % nb_model]
+        id = VEHICLE_ID[i % nb_model]
         vehicle_bp = blueprint_library.filter(id)[0]
         vehicle = world.spawn_actor(vehicle_bp, spawn)
         vehicle.set_light_state(lights)
--- sources/simulation.py
+++ sources/simulation.py
@@ -1,33 +1,33 @@
 import daynightdl as dn
-import config as conf
-import sys
+import argparse
 
 from tqdm import tqdm
 from queue import Queue, Empty
 from time import sleep
 
 
-def simulation(is_sun, confObj):
+def simulation(config):
     try:
         # ---------------------------------------------------------------------
         # INITIALIZATION
         #
         # client
-        client = dn.carla.Client(confObj.host, confObj.port)
+        client = dn.carla.Client(config.host, config.port)
         client.set_timeout(10.0)
 
         # load the specified world
         try:
-            world = client.load_world(confObj.town)
+            world = client.load_world(config.town)
         except RuntimeError:
-            print(f"{confObj.town} not found, use the current world")
+            print(f"{config.town} not found, use the current world")
             world = client.get_world()
 
         # set world mode to synchronous
-        world.apply_settings(
-                dn.carla.WorldSettings(synchronous_mode=True,
-                                       no_rendering_mode=False,
-                                       fixed_delta_seconds=0.05))
+        world.apply_settings(dn.carla.WorldSettings(
+                                synchronous_mode=True,
+                                no_rendering_mode=False,
+                                fixed_delta_seconds=0.05)
+                             )
 
         # Set up the traffic manager
         TM_PORT = 8000
@@ -36,11 +36,11 @@ def simulation(is_sun, confObj):
         traffic_manager.set_random_device_seed(1)
 
         # weather
-        dn.IMAGE_FOLDER = "DAY" if is_sun else "NIGHT"
-        dn.set_weather(world, is_sun, confObj)
+        dn.IMAGE_FOLDER = "DAY" if config.angle >= 0 else "NIGHT"
+        dn.set_weather(world, config)
 
         # set traffic and pick the first car
-        vehicle_list = dn.set_autonom_car(world, confObj, TM_PORT)
+        vehicle_list = dn.set_autonom_car(world, config, TM_PORT)
         vehicle = vehicle_list[0]
         traffic_manager.ignore_lights_percentage(vehicle, 100)
 
@@ -48,8 +48,8 @@ def simulation(is_sun, confObj):
         sensor_list = []
         sensor_queue = Queue()
 
-        cam_seg = dn.camera_init("seg", world, vehicle, sensor_queue, confObj)
-        cam_rgb = dn.camera_init("rgb", world, vehicle, sensor_queue, confObj)
+        cam_seg = dn.camera_init("seg", world, vehicle, sensor_queue, config)
+        cam_rgb = dn.camera_init("rgb", world, vehicle, sensor_queue, config)
 
         sensor_list.append(cam_seg)
         sensor_list.append(cam_rgb)
@@ -59,10 +59,10 @@ def simulation(is_sun, confObj):
         #
         world.tick()
         print(f"Recording {dn.IMAGE_FOLDER} images")
-        for dn.frame_id in tqdm(range(1, confObj.imNum+1)):
+        for dn.frame_id in tqdm(range(1, config.number+1)):
             try:
                 for _ in range(len(sensor_list)):
-                    s_frame = sensor_queue.get(block=True, timeout=2)
+                    sensor_queue.get(block=True, timeout=2)
             except Empty:
                 continue
             for _ in range(20):
@@ -85,9 +85,73 @@ def simulation(is_sun, confObj):
 
 
 if __name__ == '__main__':
-    is_sun = True if sys.argv[1] == "sun" else False
+    argparser = argparse.ArgumentParser(description=__doc__)
+    argparser.add_argument(
+        '--host',
+        default='localhost',
+        help='IP of the host server (default: localhost)'
+    )
+    argparser.add_argument(
+        '--port', '-p',
+        default=2000,
+        type=int,
+        help='TCP port to listen to (default: 2000)'
+    )
+    argparser.add_argument(
+        '--town', '-t',
+        default='town01',
+        choices=['town01', 'town02', 'town03', 'town04',
+                 'town05', 'town06', 'town07', 'town10HD'],
+        help='Selected town (default: town01)'
+    )
+    argparser.add_argument(
+        '--fov',
+        default=110,
+        type=int,
+        help='Field of view (default: 110)'
+    )
+    argparser.add_argument(
+        '--dimension', '-d',
+        nargs='+',
+        type=int,
+        default=[1920, 1080],
+        help='Image dimension (default: [1920, 1080])'
+    )
+    argparser.add_argument(
+        '--number', '-n',
+        default=100,
+        type=int,
+        help='Number of image (default: 100)'
+    )
+    argparser.add_argument(
+        '--angle', '-a',
+        default=70,
+        type=float,
+        help='Angle between the sun and the ground (default: 70)'
+    )
+    argparser.add_argument(
+        '--traffic', '-tr',
+        default=0,
+        type=int,
+        help='Traffic percentage (default: 0)'
+    )
+    argparser.add_argument(
+        '--position',
+        nargs='+',
+        type=float,
+        default=[2.5, 0, 0.7],
+        help='(x,y,z) camera position (default: [2.5, 0, 0.7])'
+    )
+    argparser.add_argument(
+        '--rotation',
+        nargs='+',
+        type=float,
+        default=[0, 0, 0],
+        help='(roll,yaw,pitch) camera rotation (default: [0, 0, 0])'
+    )
 
-    sim_conf = conf.Config(town=sys.argv[2], sim=sys.argv[3], imNum=251)
+    config = argparser.parse_args()
 
-    simulation(is_sun, sim_conf)
-    sleep(2) # allow time to save the last image
+    simulation(config)
+
+    sleep(2)  # allow time to save the last image
