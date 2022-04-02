a5904d91528df64b4c2c6897ffc2ca86f2d9b938
Yaceee <yacine.laghdiri@gmail.com>
2022-04-02 20:12:38 +0200

    gui with eel + electron

--- gui/gui.py
+++ gui/gui.py
@@ -1,4 +1,13 @@
 import eel
+import eel.browsers
+import sys
 
 eel.init('views')
-eel.start('main.html', mode="chrome")
+
+@eel.expose
+def startSimulationPy(json):
+    print(json)
+
+eel.browsers.set_path('electron', './node_modules/electron/dist/electron')
+
+eel.start('main.html', mode="electron", host="localhost", port=8000, disable_cache=True)
--- sources/config.py
+++ sources/config.py
@@ -6,6 +6,7 @@ Objet de configuration de la simulation
 """
 
 import sources.error as error
+import json
 
 HOST = "localhost"
 PORT = 2000
@@ -21,32 +22,73 @@ ANGLE_DAY = 70
 ANGLE_NIGHT = -178
 TRAFFIC = 20
 
-VEHICLE_ID = ["a2", "impala", "c3", "microlino", "charger_police", "tt", "wrangler_rubicon", "coupe", "coupe_2020", "low_rider", "charger_2020", "ambulance", "mkz_2020", "mini", "prius", "crown", "carlacola", "zx125", "nissan", "charger_police_2020", "sprinter", "etron", "leon", "t2_2021", "cybertruck", "mkz_2017", "mustang", "carlamotors", "volkswagen", "tesla", "century", "omafiets", "grandtourer", "crossbike", "ninja", "yzf", "patrol", "micra", "cooper_s"]
 
 class Config:
 
-    rgbTag = "rgb"
-    segTag = "seg"
+	rgbTag = "rgb"
+	segTag = "seg"
+
+	VEHICLE_ID = ["a2", "impala", "c3", "microlino", "charger_police", "tt", "wrangler_rubicon", "coupe", "coupe_2020", "low_rider", "charger_2020", "ambulance", "mkz_2020", "mini", "prius", "crown", "carlacola", "zx125", "nissan", "charger_police_2020", "sprinter", "etron", "leon", "t2_2021", "cybertruck", "mkz_2017", "mustang", "carlamotors", "volkswagen", "tesla", "century", "omafiets", "grandtourer", "crossbike", "ninja", "yzf", "patrol", "micra", "cooper_s"]
+
+	def __init__(self, host, port, town, fov, width, height, imNum, sun, moon, traffic, vehicle_id):
+		self.host = host
+		self.port = port
+		self.town = town
+		self.fov = fov
+		self.width = width
+		self.height = height
+		self.imNum = imNum
+		self.sun = sun
+		self.moon = moon
+		self.traffic = traffic
+		self.vehicle_id = vehicle_id
+
+	def getHost(self):
+		return self.host
+
+	def getPort(self):
+		return self.port
+
+	def getTown(self):
+		return self.town
+
+	def getFov(self):
+		return self.fov
+
+	def getWidth(self):
+		return self.width
+
+	def getHeight(self):
+		return self.height
+
+	def getImNum(self):
+		return self.imNum
+
+	def getSun(self):
+		return self.sun
+
+	def getMoon(self):
+		return self.moon
+
+	def getTraffic(self):
+		return self.traffic
+	
+	def getVehicleId(self):
+		return self.vehicle_id
+
+	def checkConfig(self):
+		error.checkConnection(self.host, self.port)
+		error.checkDimensions(self.width, self.height)
+		error.checkFov(self.fov)
+		error.checkImgNum(self.imNum)
 
 
-    def __init__(self, host, port, town, fov, width, height, imNum, sun, moon, traffic, vehicle_id):
-        self.host = host
-        self.port = port
-        self.town = town
-        self.fov = fov
-        self.width = width
-        self.height = height
-        self.imNum = imNum
-        self.sun = sun
-        self.moon = moon
-        self.traffic = traffic
-        self.vehicle_id = vehicle_id
-
-    def checkConfig(self):
-        error.checkConnection(self.host, self.port)
-        error.checkDimensions(self.width, self.height)
-        error.checkFov(self.fov)
-        error.checkImgNum(self.imNum)
+def confFromJSON(json : json):
+	decoded = json.loads(json)
+	return Config(decoded['host'], decoded['port'], decoded['town_id'], decoded['fov'],
+	decoded['width'], decoded['height'], decoded['imNum'], decoded['day'], decoded['night'],
+	decoded['traffic'], Config.VEHICLE_ID)
 
 globalConf = Config(HOST, PORT, TOWN_ID, IM_FOV, IM_WIDTH, IM_HEIGHT, IM_NUMBER,
-                    ANGLE_DAY, ANGLE_NIGHT, TRAFFIC, VEHICLE_ID)
+					ANGLE_DAY, ANGLE_NIGHT, TRAFFIC, Config.VEHICLE_ID)
+
