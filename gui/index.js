c641f4234861dce6f73fd5dc685c37fd740a6b05
Yaceee <yacine.laghdiri@gmail.com>
2022-04-05 13:52:45 +0200

    ajout champs ui + simThread

--- gui/index.js
+++ gui/index.js
@@ -5,17 +5,14 @@ function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 800,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
+    height: 600
   })
 
   // and load the index.html of the app.
   mainWindow.loadURL('http://localhost:8000/main.html')
 
   // Open the DevTools.
-   mainWindow.webContents.openDevTools()
+  // mainWindow.webContents.openDevTools()
 }
 
 // This method will be called when Electron has finished
